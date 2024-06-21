87a5c45d394f88fbefc7e3c02f55443272eb1f48
Deepak <Deepak.kumar2@conduent.com>
2024-06-21 21:11:13 +0530

    WebView In Android

---
+++ app/src/main/java/com/example/rishabh_app/WebActivity.java
@@ -0,0 +1,34 @@
+package com.example.rishabh_app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_web);
+        webView=findViewById(R.id.webview);
+
+        // Enable JavaScript (optional)
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Load a web page into WebView
+        webView.loadUrl("https://www.android.com/intl/en_in/");
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
